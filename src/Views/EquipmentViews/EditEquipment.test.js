3f9486c5637fd60309ae7d4ca06c75ea6b1d9a87
Diazu27 <AndresDiazu2001@gmail.com>
2022-12-06 10:33:52 -0600

    Deleted

--- src/Views/EquipmentViews/EditEquipment.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react"
-import { EditEquipment } from "./EditEquipment"
-
-test('Does the EditEquipment component render successfully', async()=>{
-    render(<EditEquipment/>);
-    
-})
