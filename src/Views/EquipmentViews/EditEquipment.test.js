36bcc934066d65f62d8dbc3b452ff9c999b74e37
Diazu27 <AndresDiazu2001@gmail.com>
2022-12-05 22:59:47 -0600

    Add test to equipment modules

---
+++ src/Views/EquipmentViews/EditEquipment.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react"
+import { EditEquipment } from "./EditEquipment"
+
+test('Does the EditEquipment component render successfully', async()=>{
+    render(<EditEquipment/>);
+})
